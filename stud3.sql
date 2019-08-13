08efe4149733dc3b5fe0dc2326c0ea599a4470e1
ShivaniTamilselvam <53708830+ShivaniTamilselvam@users.noreply.github.com>
2019-08-13 14:37:51 +0530

    Create stud3.sql

---
+++ stud3.sql
@@ -0,0 +1,4 @@
+--SELECT * FROM demo;
+CREATE TABLE Stud3('StudID','Regnum','Name','Gender');
+insert into Stud3('StudID','Regnum','Name','Gender')values('26','746732647864','Veer','M');
+SELECT * FROM Stud3;
