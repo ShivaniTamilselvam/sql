cefea4c345f6d7717767c71ae2b94d90f5ec7999
ShivaniTamilselvam <53708830+ShivaniTamilselvam@users.noreply.github.com>
2019-08-14 11:17:11 +0530

    Create stud10.py

---
+++ stud10.py
@@ -0,0 +1,8 @@
+--SELECT * FROM demo;
+CREATE TABLE Stud10('StudID','Regnum','Name','Gender');
+insert into Stud10('StudID','Regnum','Name','Gender')values('26','746732647864','Veer','M');
+insert into Stud10('StudID','Regnum','Name','Gender')values('27','134566789045','Berry','F');
+insert into Stud10('StudID','Regnum','Name','Gender')values('28','324566685643','Maya','F');
+insert into Stud10('StudID','Regnum','Name','Gender')values('29','2457980067554','Priya','F');
+insert into Stud10('StudID','Regnum','Name','Gender')values('30','1356789776554','Sandy','M');
+SELECT * FROM Stud10;
